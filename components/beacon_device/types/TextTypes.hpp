f507ec9bde6276d8ab6983bed1cb99f72a45f7d8
IJssel <ijssel@synapt.nl>
2026-05-17 18:56:39 +0200

    Reworked textSlotConfig for fallback

--- components/beacon_device/types/TextTypes.hpp
+++ components/beacon_device/types/TextTypes.hpp
@@ -1,23 +1,28 @@
 
 
-enum class TextSource {
+enum class TextSourceType {
     NONE=0,
     STATIC,
     SHORT_NAME,
     LONG_NAME,
     VARIABLE_FIELD,
     TIME,
-    COUNTDOWN,
+    // COUNTDOWN, // TODO
+};
+
+struct TextSource {
+    TextSourceType type;
+    char staticText[32]; // For STATIC source
+    uint8_t fieldIndex; // For VARIABLE_FIELD source
+};
+
+struct AlertSourceConfig {
+    uint8_t alertIndex; // For alert sources, which alert to respond to.
+    DeviceAlertTarget alertTarget;
 };
 
 struct TextSlotConfig {
-    TextSource source;
-    struct AlertSourceConfig {
-        uint8_t alertIndex; // For alert sources, which alert to respond to.
-        DeviceAlertTarget alertTarget;
+    TextSource sources[4]; // Source + fallbacks if empty
 
-    } alert;
-    char staticText[32]; // For STATIC source
-    uint8_t fieldIndex; // For VARIABLE_FIELD source
-
-}
+    std::optional<AlertSourceConfig> alert; // nullptr if disabled.
+};
